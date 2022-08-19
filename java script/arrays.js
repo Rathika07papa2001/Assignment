a397d71723409a29d95ae5d7e42ac71f6622bf54
Rathika <ratharathik2001@gmail.com>
2022-08-19 14:18:52 +0530

    afternoon

---
+++ java script/arrays.js
@@ -0,0 +1,8 @@
+<script>
+    const cars = [];
+    cars[0] ="saab";
+    cars[1] ="volvo";
+    cars [2] ="BMW";
+    doucments.getElementssByID("demo").inner
+    HTML=cars;
+</script>
--- java script/keys.js
+++ java script/keys.js
@@ -1,6 +1,6 @@
-let emp={id:101,Name:Rathika,sal:200000}
+let emp={id:101,Name:Rathika,sal:20000}
+let keys=Object.key(emp)
+console.log(keys.length>0)
 //console.log(emp.length)
 //console.log(emp.email)
 
-let keys=Object,key(emp)
-console.log(keys.length>0)
